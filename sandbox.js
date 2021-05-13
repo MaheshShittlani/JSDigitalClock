27d9b7c3d2ad8ed942a01d7c61063058efa1e8d6
Mahesh Shitlani <mahesh.s@ninehertzindia.com>
2021-05-13 21:46:49 +0530

    digital clock done

---
+++ sandbox.js
@@ -0,0 +1,19 @@
+const clock = document.querySelector('#clock');
+
+const tick = () => {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    const html = `
+        <span>${hours}</span> : 
+        <span>${minutes < 10 ? '0'+minutes: minutes}</span> :
+        <span>${seconds < 10 ? '0'+seconds: seconds}</span>
+    `;
+
+    clock.innerHTML = html;
+}
+
+setInterval(tick, 1000);
+
